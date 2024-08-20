8624616860a95020a5ec9c1db48045e7a9622ce9
Mohammed Anaz <152995022+mohammedanaz@users.noreply.github.com>
2024-08-20 10:03:18 +0530

    Time: 275 ms (83.00%) | Memory: 16.6 MB (39.39%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_around_center(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        longest_palindrome = ""
+        for i in range(len(s)):
+            
+            odd_palindrome = expand_around_center(i, i)
+            if len(odd_palindrome) > len(longest_palindrome):
+                longest_palindrome = odd_palindrome
+
+            even_palindrome = expand_around_center(i, i + 1)
+            if len(even_palindrome) > len(longest_palindrome):
+                longest_palindrome = even_palindrome
+
+        return longest_palindrome
