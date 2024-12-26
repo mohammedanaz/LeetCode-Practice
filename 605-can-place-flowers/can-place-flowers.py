8bdad786ec80f4026f4cdc17c0caeee1834a6c23
Mohammed Anaz <152995022+mohammedanaz@users.noreply.github.com>
2024-12-26 10:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (7.10%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -3,14 +3,17 @@ class Solution:
         new_plots = 0
         length = len(flowerbed)
         i = 0
-        for i in range(length):
+        while i < length:
             if flowerbed[i] == 0:
-                prev = i == 0 or flowerbed[i - 1] == 0
-                next = i == length - 1 or flowerbed[i + 1] == 0
-                if prev and next:
-                    new_plots += 1
+                if (i==0 or flowerbed[i-1]==0) and (i==length-1 or flowerbed[i+1]==0):
                     flowerbed[i] = 1
+                    new_plots += 1
                     if new_plots >= n:
                         return True
+                    i += 2
+                else:
+                    i += 1
+            else:
+                i += 1
         return new_plots >= n
         
